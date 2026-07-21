5ea06e33a5a5d3475a5488a5efb305652e03b520
Wycliffe Maina <muchumimaina94@gmail.com>
2026-07-21 11:53:16 +0300

    Included stk router

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, status
 from api.db.database import Base, engine
+from api.routers import stk_routes as router
 
 app = FastAPI(title="STK push API", description="A FastAPI application for M-Pesa STK Push", version="1.0.0")
+app.include_router(router)
 
 # Create tables
 Base.metadata.create_all(bind=engine)
@@ -9,3 +11,4 @@ Base.metadata.create_all(bind=engine)
 @app.get("/", status_code=status.HTTP_200_OK)
 def root():
     return {"message": "Hello world, welcome to STK push API"}
+
