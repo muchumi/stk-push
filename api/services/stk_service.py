b4e713dcd72aea8ea0bfa6ad165f4ccb417b78fd
Wycliffe Maina <muchumimaina94@gmail.com>
2026-07-26 12:48:47 +0300

    Added status codes for Service unavailable and no access token errors

--- api/services/stk_service.py
+++ api/services/stk_service.py
@@ -1,5 +1,6 @@
 import base64
 import requests
+from fastapi import HTTPException, status
 from api.core.config import settings
 
 def get_access_token():
@@ -14,14 +15,19 @@ def get_access_token():
     headers={
         "Authorization": f"Basic {encoded_credentials}"
     }
-
-    response=requests.get(
-        settings.OAUTH_URL,
-        headers=headers,
-        timeout=
-    )
-    response.raise_for_status()
-    token=response.json()["access_token"]
+    try:
+        response=requests.get(
+            settings.OAUTH_URL,
+            headers=headers,
+            timeout=10
+        )
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=f"Failed to connect to Safaricom Daraja API Service: {str(e)}")
+    data=response.json()
+    token=data.get("access_token")
+    if not token:
+        raise HTTPException(status_code=status.HTTP_502_BAD_GATEWAY, detail="No access token returned from Safaricom Daraja API")
     return token
 
 
