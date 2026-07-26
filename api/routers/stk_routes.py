22c30c56c2d4501036248034c307f92d00582645
Wycliffe Maina <muchumimaina94@gmail.com>
2026-07-26 19:57:43 +0300

    Added generate_timestamp and generate_password helper function

--- api/routers/stk_routes.py
+++ api/routers/stk_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request
 from api.schemas.stk_schema import STKPushRequest
+from api.services.stk_helpers import generate_timestamp, generate_password
 
 router = APIRouter(prefix="/stk", tags=["STK Push"])
 
@@ -9,3 +10,4 @@ def stk_push(request: STKPushRequest):
         "message": "STK push initiated",
         "data":request.model_dump()
     }
+
