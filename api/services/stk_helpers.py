ce68ea1cb78810bf4a2e7fb98be9b282d0204c32
Wycliffe Maina <muchumimaina94@gmail.com>
2026-07-26 22:39:18 +0300

    Added reusable helper functions

--- api/services/stk_helpers.py
+++ api/services/stk_helpers.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 import base64
-from api.core.config import settings
+from api.core.config import setting
 
 """
+    Providing reusable helper functions.
     Generate a timestamp in the format required by Safaricom Daraja API (YYYYMMDDHHMMSS).
 """
 def generate_timestamp()->str:
@@ -11,8 +12,8 @@ def generate_timestamp()->str:
 
 def generate_password(timestamp:str)->str:
     password=(
-        settings.BUSINESS_SHORT_CODE +
-        settings.PASSKEY +
+        setting.BUSINESS_SHORT_CODE +
+        setting.PASSKEY +
         timestamp
     )
     return base64.b64encode(password.encode()).decode() 
