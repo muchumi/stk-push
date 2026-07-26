940c5785d592d4f7cf02146e356768ac197fdad3
Wycliffe Maina <muchumimaina94@gmail.com>
2026-07-26 22:37:40 +0300

    Wrapped all config settings into Settings class

--- api/core/config.py
+++ api/core/config.py
@@ -3,12 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CONSUMER_KEY = os.getenv("CONSUMER_KEY")
-CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+class Settings:
+    CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+    CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+    SHORTCODE = os.getenv("SHORTCODE")
+    PASSKEY = os.getenv("PASSKEY")
+    CALLBACK_URL = os.getenv("CALLBACK_URL")
+    BASE_URL="https://sandbox.safaricom.co.ke"
 
-SHORTCODE = os.getenv("SHORTCODE")
-PASSKEY = os.getenv("PASSKEY")
 
-CALLBACK_URL = os.getenv("CALLBACK_URL")
-
-BASE_URL="https://sandbox.safaricom.co.ke"
+setting=Settings()
