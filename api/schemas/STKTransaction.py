9045ac79a294c853a7c26fe74aefbf8f5b668337
Wycliffe Maina <muchumimaina94@gmail.com>
2026-08-02 12:10:59 +0300

    Added STKTransaction response schema

---
+++ api/schemas/STKTransaction.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, ConfigDict
+
+"""
+    STK Transaction Response Model.
+    It is used to validate the response data from the database and ensure that it conforms to the expected structure.
+"""
+class STKTransactionResponse(BaseModel):
+    id: int
+    phoneNumber: str
+    amount: float
+    merchant_request_id: str | None
+    checkout_request_id: str | None
+    status: str
+    result_desc: str | None
+
+    model_config = ConfigDict(from_attributes=True)
+
