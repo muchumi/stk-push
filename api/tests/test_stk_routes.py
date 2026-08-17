9517bb0841cf696186f4cf0b7e8bf072b8fd9f95
Wycliffe Maina <muchumimaina94@gmail.com>
2026-08-17 10:45:24 +0300

    Testing the root endpoint

---
+++ api/tests/test_stk_routes.py
@@ -0,0 +1,6 @@
+def test_root(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json()=={
+        "message": "Hello world, welcome to STK push API"
+    }
